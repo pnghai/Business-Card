9f5960ae197f545222ddf7edb97cacd83deae925
hai <pnghai@gmail.com>
2020-09-15 18:49:03 +0700

    first version

--- Business Card/ContentView.swift
+++ Business Card/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(alignment: .center) {
+            Image("paulos-nice-photo").resizable().scaledToFit().clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 3))
+                .shadow(radius: 5)
+            
+            VStack {
+                Text("Hai Phan Nguyen").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.white)
+                Text("https://github.io/pnghai")
+                    .font(.subheadline)
+                    .foregroundColor(.white)
+                HStack {
+                    Image(systemName: "t.square.fill").foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    Text(": pnghai@gmail.com")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                        .bold().italic()
+                }
+            }
+        }
+        .background(Color.orange).cornerRadius(8)
+        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/).shadow(radius: 5)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .padding(5.0)
     }
 }
