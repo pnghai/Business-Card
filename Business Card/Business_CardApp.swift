21a079e86c75a6a7952ac87dc37acdf30a7aaa16
hai <pnghai@gmail.com>
2020-09-15 17:39:08 +0700

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by admin on 15/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Business Card/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Business Card
+//
+//  Created by admin on 15/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
